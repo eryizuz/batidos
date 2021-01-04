f26c0d2d21c33be91afd5cb1385dc85483f1d3e3
eryizuz <69830369+eryizuz@users.noreply.github.com>
2021-01-04 12:48:56 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const path = require('path');
 const express = require('express');
-const app = express();
+const path = require('path');
 
-const publicPath = path.join(__dirname, 'public');
+const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(publicPath));
-app.get('*', (req, res) => {
-   res.sendFile(path.join(publicPath, 'index.html'));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(port, () => {
-   console.log('Server is up!');
-});
+app.listen(port);
